20f12ee024ccf33b1116e39dc3187cefcb994d69
amrelmeslimany-portfolio <aamr_786@hotmail.com>
2022-05-19 17:35:44 +0200

    Create all pages V1

--- js/app.js
+++ js/app.js
@@ -35,6 +35,9 @@ $(function () {
   ];
   const underlineLetters = ["ج", "ح", "خ", "ع", "غ", "م"];
 
+  // Articles Section
+  const bestArticles = $(".bestArticles .article-item");
+
   // Window resize
   $(window).on("resize", () => {
     // set hieght for adv
@@ -42,26 +45,48 @@ $(function () {
   });
 
   // Insert Letters to Body
-  arabicLetters.forEach(function (letter) {
-    if (underlineLetters.includes(letter))
-      lettersWrap.append(
-        `<span class="letter-badge badge"><span class="underline-letter">${letter}</span></span>`
-      );
-    else
-      lettersWrap.append(
-        `<span class="letter-badge badge"><span>${letter}</span></span>`
-      );
-  });
+  if (lettersWrap.length) {
+    arabicLetters.forEach(function (letter) {
+      if (underlineLetters.includes(letter))
+        lettersWrap.append(
+          `<span class="letter-badge badge"><span class="underline-letter">${letter}</span></span>`
+        );
+      else
+        lettersWrap.append(
+          `<span class="letter-badge badge"><span>${letter}</span></span>`
+        );
+    });
+  }
 
   // Set height of adv
   setADVHeight();
+
+  // Truncate Article title and Desc
+  if (bestArticles.length) {
+    bestArticles.each(function () {
+      let title = $(this).find(".article-title");
+      let desc = $(this).find(".article-desc");
+
+      truncate(title, 60);
+      truncate(desc, 600);
+    });
+  }
+
+  // ReUsed Functions
   function setADVHeight() {
     // Set Height of adv
-    $(".3double-height").each(function () {
-      let width = $(this).width();
+    if ($(".3double-height").length && sideAdv.length) {
+      $(".3double-height").each(function () {
+        let width = $(this).width();
 
-      $(this).height(`${width / 3}px`);
-    });
-    sideAdv.height(`${sideAdv.width() * 2}px`);
+        $(this).height(`${width / 3}px`);
+      });
+      sideAdv.height(`${sideAdv.width() * 2}px`);
+    }
+  }
+
+  function truncate(text, sliceNumber) {
+    text.text().trim().length > sliceNumber &&
+      text.text(text.text().trim().substring(0, sliceNumber) + "...");
   }
 });
